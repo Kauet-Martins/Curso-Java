ecf963bd647ffc570204e1c114449e39fc0df480
Kauet-Martins <kauetdamotta@gmail.com>
2022-08-05 20:35:51 -0300

    Primeiro Passos

---
+++ PrimeirosPassos.java
@@ -0,0 +1,14 @@
+public class PrimeirosPassos {
+
+    // Metodo de Entrada de dados //
+    public static void main(String [] args){
+
+     // Codigo que escreve Hello Word
+     System.out.println("Hello Word");
+
+     // Codigo que escreve a soma de dois numero
+     System.out.println(4 + 6);
+
+    }
+
+}
