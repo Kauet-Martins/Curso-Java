9c7cfb8fd525eb8dccdb89bc1e47216543ce2048
Kauet-Martins <kauetdamotta@gmail.com>
2022-08-08 18:31:10 -0300

    Funcoes

---
+++ Funcoes.java
@@ -0,0 +1,23 @@
+public class Funcoes {
+
+    public static void main(String[] args){
+        sum(8, 2); // ARGUMENTOS
+    }
+
+    // PARAMETRO PARA CHAMAR UMA DETERMINADA FUNCAO
+    // DEFININDO AS VARIAVEIS COMO INTEIRAS
+    static void sum(int x, int y){
+
+        // IMPRIMINDO A SOMA DE X (8) Y (2) DEFINIDOS ANTERIORMENTE
+        System.out.println(x + y);
+
+        // IMPRIMINDO A SUBTRACAO DE X (8) Y (2) DEFINIDOS ANTERIORMENTE
+        System.out.println(x - y);
+
+        // IMPRIMINDO A DIVISAO DE X (8) Y (2) DEFINIDOS ANTERIORMENTE
+        System.out.println(x / y);
+
+        // IMPRIMINDO A MULTIPLICACAO DE X (8) Y (2) DEFINIDOS ANTERIORMENTE
+        System.out.println(x * y);
+    }
+}
